563168c1d0a799ea41f06fa96957a4daf3da1945
Aki Nakamura <nakamura5akihito@gmail.com>
2014-02-25 19:26:36 +0900

    Tests clean-up.

--- src/test/java/jp/go/aist/six/oval/core/repository/OvalRepositoryImplTest.java
+++ src/test/java/jp/go/aist/six/oval/core/repository/OvalRepositoryImplTest.java
@@ -3,7 +3,6 @@ package jp.go.aist.six.oval.core.repository;
 import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
 import java.io.File;
-import java.io.FileInputStream;
 import java.util.List;
 import jp.go.aist.six.oval.core.OvalTestBase;
 import jp.go.aist.six.oval.core.SixOvalContext;
@@ -61,7 +60,7 @@ public class OvalRepositoryImplTest
      * @see http://oval.mitre.org/repository/about/testcontent.html
      */
     @RunWith( Theories.class )
-    public static class OvalTestContent510Repository
+    public static class OvalTestContent510SaveAndFind
     extends OvalTestBase
     {
         public static final String  INPUT_ROOTDIR_PATH = "src/test/resources/data/oval/oval-test-content_5_10_1_3";
@@ -78,7 +77,7 @@ public class OvalRepositoryImplTest
 
 
 
-        public OvalTestContent510Repository()
+        public OvalTestContent510SaveAndFind()
         {
             super( SixOvalContext.repository() );
         }
@@ -109,7 +108,7 @@ public class OvalRepositoryImplTest
      * @see http://oval.mitre.org/repository/
      */
     @RunWith( Theories.class )
-    public static class MitreOvalDef510Repository
+    public static class MitreOvalDef510SaveAndFind
     extends OvalTestBase
     {
         public static final String  INPUT_ROOTDIR_PATH = "src/test/resources/data/oval/mitre-oval-def-5_10";
@@ -123,7 +122,7 @@ public class OvalRepositoryImplTest
 
 
 
-        public MitreOvalDef510Repository()
+        public MitreOvalDef510SaveAndFind()
         {
             super( SixOvalContext.repository() );
         }
@@ -155,7 +154,7 @@ public class OvalRepositoryImplTest
      * @see http://oval.mitre.org/repository/
      */
     @RunWith( Theories.class )
-    public static class RedhatOvalDef53Repository
+    public static class RedhatOvalDef53SaveAndFind
     extends OvalTestBase
     {
         public static final String  INPUT_ROOTDIR_PATH = "src/test/resources/data/oval/redhat-oval-def-5_3";
@@ -168,7 +167,7 @@ public class OvalRepositoryImplTest
 
 
 
-        public RedhatOvalDef53Repository()
+        public RedhatOvalDef53SaveAndFind()
         {
             super( SixOvalContext.repository() );
         }
@@ -194,149 +193,13 @@ public class OvalRepositoryImplTest
 
 
 
-	////////////////////////////////////////////////////////////////////////////
     /**
-     * support function.
-     */
-    public static void saveOvalDefinitions(
-                    final OvalRepository repository,
-                    final OvalDefinitions oval_defs
-                    )
-    {
-        repository.saveOvalDefinitions( oval_defs );
-        for (DefinitionType def : oval_defs.getDefinitions().getDefinition()) {
-            String  oval_id = def.getOvalId();
-            DefinitionType  p_def = repository.findDefinitionById( oval_id );
-            assertThat( p_def, is( notNullValue() ) );
-        }
-    }
-
-
-
-    /**
-     * Repository test: Mitre repository --- save & find.
-     *
-     * @see http://oval.mitre.org/repository/
-     */
-    @RunWith( Theories.class )
-    public static class SaveAndFindMitreOvalRepositoryContent
-    extends OvalTestBase
-    {
-
-        public static final String  INPUT_ROOTDIR_PATH = "src/test/resources/data/oval5/mitre";
-
-
-        @DataPoints
-        public static String[]  INPUT_SUBDIR_PATHS = new String[] {
-            "aix",
-            "esx",
-            "linux",
-            "macos",
-            "solaris",
-//            "unix"
-            "windows"
-        };
-
-
-
-        public SaveAndFindMitreOvalRepositoryContent()
-        {
-            super( SixOvalContext.repository() );
-        }
-
-
-
-        @Theory
-        public void testSaveDefinition(
-                        final String input_subdir_path
-                        )
-        throws Exception
-        {
-            File[]  input_xml_files = _listXmlFiles( INPUT_ROOTDIR_PATH, input_subdir_path );
-            for (File  input_xml_file : input_xml_files) {
-                System.out.println( "OVAL Document: filepath=" + input_xml_file );
-                /* (1) unmarshal */
-                Object  obj = _getXmlMapper().unmarshal( new FileInputStream( input_xml_file ) );
-                if (OvalDefinitions.class.isInstance( obj )) {
-                    OvalDefinitions  oval_defs = OvalDefinitions.class.cast( obj );
-                    saveOvalDefinitions( _getRepository(), oval_defs );
-//                    _getRepository().saveOvalDefinitions( oval_defs );
-                }
-
-            }
-        }
-
-    }
-    //
-
-
-
-    /**
-     * Repository test: OVAL test content --- save & find.
+     * TEST: OVAL test content / definitions / 5.10
      *
      * @see http://oval.mitre.org/repository/about/testcontent.html
      */
     @RunWith( Theories.class )
-    public static class SaveAndFindOvalTestContent
-    extends OvalTestBase
-    {
-
-        public static final String  INPUT_ROOTDIR_PATH = "src/test/resources/data/oval5/ovaltc-5.10.1.3";
-
-
-        @DataPoints
-        public static String[]  INPUT_SUBDIR_PATHS = new String[] {
-            "definitions",
-            "independent",
-            "linux",
-            "macos",
-            "solaris",
-            "unix",
-            "windows"
-        };
-
-
-
-        public SaveAndFindOvalTestContent()
-        {
-            super( SixOvalContext.repository() );
-        }
-
-
-
-        @Theory
-        public void testSaveDefinition(
-                        final String input_subdir_path
-                        )
-        throws Exception
-        {
-            File[]  input_xml_files = _listXmlFiles( INPUT_ROOTDIR_PATH, input_subdir_path );
-            for (File  input_xml_file : input_xml_files) {
-                System.out.println( "OVAL Document: filepath=" + input_xml_file );
-                /* (1) unmarshal */
-                Object  obj = _getXmlMapper().unmarshal( new FileInputStream( input_xml_file ) );
-                if (OvalDefinitions.class.isInstance( obj )) {
-                    OvalDefinitions  oval_defs = OvalDefinitions.class.cast( obj );
-                    saveOvalDefinitions( _getRepository(), oval_defs );
-//                  _getRepository().saveOvalDefinitions( oval_defs );
-                }
-
-            }
-        }
-
-    }
-    //
-
-
-
-
-    /**
-     * Repository test: OVAL test content --- search.
-     *
-     * @see http://oval.mitre.org/repository/about/testcontent.html
-     */
-    @RunWith( Theories.class )
-    public static class SearchOvalTestContent
+    public static class OvalTestContent510Search
     extends OvalTestBase
     {
 
@@ -373,7 +236,7 @@ public class OvalRepositoryImplTest
 
 
 
-        public SearchOvalTestContent()
+        public OvalTestContent510Search()
         {
             super( SixOvalContext.repository() );
         }
